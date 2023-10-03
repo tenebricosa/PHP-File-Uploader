82cd7ebd2259e750088e0d6df8b9d52b1db02d5f
Nastya Kataeva <elliot.wrt@gmail.com>
2023-10-03 11:00:23 +0200

    Minor enhancements

--- PHP Uploader with Uploadcare/uploadHandling.php
+++ PHP Uploader with Uploadcare/uploadHandling.php
@@ -1,29 +1,29 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    use Uploadcare\Configuration;
+    use Uploadcare\Api as UploadcareApi;
+
+    $configuration = Configuration::create(
+        'c76afb86b9c735c1c2f9',
+        '95e978da240ec4af88a7'
+    );
+
+    $uploader = (new UploadcareApi($configuration))->uploader();
+
+    $fileInfo = $uploader->fromPath(
+        $_FILES["fileToUpload"]["tmp_name"],
+        null,
+        null,
+        'auto'
+    );
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
 <?php
-
-require_once 'vendor/autoload.php';
-
-use Uploadcare\Configuration;
-use Uploadcare\Api as UploadcareApi;
-
-$configuration = Configuration::create(
-    'YOUR_PUBLIC_KEY',
-    'YOUR_SECRET_KEY'
-);
-
-$uploader = (new UploadcareApi($configuration))->uploader();
-
-$fileInfo = $uploader->fromPath(
-    $_FILES["fileToUpload"]["tmp_name"],
-    null,
-    null,
-    'auto'
-);
-
-echo "<img width='400' src='https://ucarecdn.com/" . $fileInfo . "/-/preview/' />";
-
+    echo "<img width='400' src='https://ucarecdn.com/" . $fileInfo . "/-/preview/' />";
 ?>
 </body>
 </html>
